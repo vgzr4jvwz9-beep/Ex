79d3c9565638b6c716e2c0ef1803228cffe6b3d8
vgzr4jvwz9-beep <vgzr4jvwz9@privaterelay.appleid.com>
2026-05-24 17:49:38 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME = "workout-hike-tracker-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))));
-self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
