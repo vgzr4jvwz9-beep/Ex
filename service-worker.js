fb5eab7c51a77c2a20c1978168e6d3825a43da72
vgzr4jvwz9-beep <vgzr4jvwz9@privaterelay.appleid.com>
2026-05-24 17:51:36 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE='workout-builder-v2';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
